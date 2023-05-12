3f4592ca111974c09b243ee8c417bce156634c0c
Mathinez94 <audumathias@gmail.com>
2023-05-12 17:00:19 +0100

    edited navbar and added css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './copmponents/navbar/Navbar';
+import Home from './pages/home/Home';
+import Stories from './pages/stories/Stories';
+import Predictions from './pages/predictions/Predictions';
+import Livescore from './pages/livescore/Livescore';
+import About from './pages/about/About';
+import Contact from './pages/contact/Contact';
+
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/stories' element={<Stories />}/>
+        <Route path='/predictions' element={<Predictions />}/>
+        <Route path='/livescore' element={<Livescore />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+      </Routes>
     </div>
   );
 }
--- src/copmponents/navbar/Navbar.js
+++ src/copmponents/navbar/Navbar.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import "./Navbar.css"
-import { NavLink,Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
+import {FaUserNinja} from 'react-icons/fa'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <div className='logo'>
-           <Link><p>MathiSpot</p></Link> 
-        </div>  
-        <nav className='navlinks'>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/stories'>Stories</NavLink>
-            <NavLink to='/predictions'>Predictions</NavLink>
-            <NavLink to='/livescore'>Livescore</NavLink>
-            <NavLink to='/about'>About</NavLink>
-            <NavLink to='/contact'>Contact</NavLink>
-        </nav>  
+         <div className="logo">
+            <FaUserNinja className='logo-logo'/>
+               <Link to='/' ><span className='logo-text'>MathiSpot</span></Link> 
+            </div>
+            <nav >
+                <NavLink to='/' className='navlinks'>Home</NavLink>
+                <NavLink to='/stories' className='navlinks'>Stories</NavLink>
+                <NavLink to='/predictions' className='navlinks'>Predictions</NavLink>
+                <NavLink to='/livescore' className='navlinks'>Livescore</NavLink>
+                <NavLink to='/about' className='navlinks'>About</NavLink>
+                <NavLink to='/contact' className='navlinks'>Contact</NavLink>
+            </nav>
     </div>
   )
 }
