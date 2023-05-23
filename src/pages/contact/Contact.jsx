db395ed7bc0cbdaed508af2d6da11da550b9c7b8
Mathinez94 <audumathias@gmail.com>
2023-05-23 10:50:13 +0100

    added contacts option to the contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import './Contact.css'
+import { FaInstagram, FaTwitter, FaTelegramPlane, FaWhatsapp, FaUserNinja, FaFacebook } from 'react-icons/fa'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <FaInstagram className='contact-icons'/><p>Instagram</p>
+      <FaTelegramPlane className='contact-icons'/><p>Telegram</p>
+      <FaTwitter className='contact-icons'/><p>Twitter</p>
+      <FaWhatsapp className='contact-icons'/><p>Whatsapp</p>
+      <FaFacebook className='contact-icons'/><p>Instagram</p>
+    </div>
   )
 }
 
