54d3b496abc4918818a8372345bad11e372a51c6
Mathinez94 <audumathias@gmail.com>
2023-05-30 03:44:22 +0100

    stories page complete

--- src/pages/stories/Stories.jsx
+++ src/pages/stories/Stories.jsx
@@ -1,16 +1,52 @@
-import React from 'react'
-// import { useEffect, useState } from 'react'
+import axios from 'axios'
+import './Stories.css'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Stories = () => {
-  // const [data, setData] = useState('')
+  const [data, setData] = useState([])
+  useEffect(() => {
 
-  // useEffect(() => {
-  //   fetch('https://catfact.ninja/facts')
-  //   .then(res => res.json())
-  //   .then(data => console.log(data.data[0].fact))
-  // },[])
+            const options =  {
+              method: 'GET',
+              url: 'https://allscores.p.rapidapi.com/api/allscores/news',
+              params: {
+                sport: '1',
+                timezone: 'America/Chicago',
+                langId: '1'
+              },
+              headers: {
+                'X-RapidAPI-Key': '9a6c81044bmshd1b5d03d345b732p1ef8f8jsn6f444d93d598',
+                'X-RapidAPI-Host': 'allscores.p.rapidapi.com'
+              }
+            };
+            
+            try {
+            axios.request(options).then((res) => {
+              setData(res.data.news)
+              console.log(res.data.news);
+            })
+            } catch (error) {
+              console.error(error);
+            }
+      },[])
+  
   return (
-    <div>Stories</div>
+    <div className='stories'>
+      {data.map(news => {
+        return <div className="newsContainer" key={news.id}>
+          <div className="newsContent">
+            <Link to={news.url} target='blank' className='link'>
+              <p className='title'>{news.title}</p>
+              <img src={news.image} alt="#"  className='storiesImage'/>
+              <span className='publishDate'>{news.publishDate}</span>
+            </Link>
+
+          </div>
+        </div>
+      })}
+    </div>
   )
 }
 
