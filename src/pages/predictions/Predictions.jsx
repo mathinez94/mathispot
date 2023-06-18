528e58a2e9cf8dbe1960f58f7c68ff46874bfd4f
Mathinez94 <audumathias@gmail.com>
2023-06-18 11:56:51 +0100

    added todays prediction

--- src/pages/predictions/Predictions.jsx
+++ src/pages/predictions/Predictions.jsx
@@ -6,15 +6,15 @@ import PredictionTableContent from './PredictionTableContent'
 
 const Predictions = () => {
   const [tableRows, setTableRows] = useState([
-    {  username: 'Mathinez', country: 'euro', teams: 'andorra vs switzerland', prediction: 'ht away'},
-    {  username: 'Mathinez', country: 'euro', teams: 'denmark vs northern ireland', prediction: 'home'},
-    {  username: 'Mathinez', country: 'euro', teams: 'gilbrata vs france', prediction: 'ht away'},
-    {  username: 'Mathinez', country: 'euro', teams: 'latvia vs turkey', prediction: 'ov 1.5'},
-    {  username: 'Mathinez', country: 'euro', teams: 'north macedonia vs ukrain', prediction: '0v 1.5'},
-    {  username: 'Mathinez', country: 'euro', teams: 'wales vs armenia', prediction: 'home'},
-    {  username: 'Mathinez', country: 'euro', teams: 'wales vs armenia', prediction: 'ht away'},
-    {  username: 'Mathinez', country: 'friendlies', teams: 'serbia vs jordan', prediction: 'home'},
-    {  username: 'Mathinez', country: 'friendlies', teams: 'colombia vs iraq', prediction: 'home'}
+    {  username: 'Mathinez', country: 'nations league', teams: 'netherland vs italy', prediction: 'draw/away'},
+    {  username: 'Mathinez', country: 'nations league', teams: 'croatia vs spain', prediction: 'ov1.5'},
+    {  username: 'Mathinez', country: 'afcon', teams: 'congo vs mali', prediction: 'draw/away'},
+    {  username: 'Mathinez', country: 'afcon', teams: 'uganda vs algeria', prediction: 'ov 1.5'},
+    {  username: 'Mathinez', country: 'afcon', teams: 'Sierra Leone vs nigeria', prediction: '0v 1.5'},
+    {  username: 'Mathinez', country: 'concacaf', teams: 'panama vs mexico', prediction: 'ov1.5'},
+    {  username: 'Mathinez', country: 'friendlies', teams: 'venezuela vs guatemala', prediction: 'home'},
+    {  username: 'Mathinez', country: 'friendlies', teams: 'singapore vs solomon island', prediction: 'home'},
+    {  username: 'Mathinez', country: 'friendlies', teams: 'egypt vs south sudan', prediction: 'home'}
   ]);
 
   const  [ showAddTable, setShowAddTable ] = useState(false)
