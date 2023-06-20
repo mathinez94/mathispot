6911bcaaba82c9577de58477a15d5e2a676c3352
Mathinez94 <audumathias@gmail.com>
2023-06-20 15:49:44 +0100

    local storage to prediction

--- src/pages/predictions/Predictions.jsx
+++ src/pages/predictions/Predictions.jsx
@@ -1,23 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Predictions.css'
 import { useState } from 'react'
 import PredictionTable from './PredictionTable'
 import PredictionTableContent from './PredictionTableContent'
 
+const retrieveTableRows = () => {
+  const tableRowsData = localStorage.getItem('tableRows');
+  if(tableRowsData){
+    return JSON.parse(tableRowsData)
+  }else{
+    return []
+  }
+}
+
 const Predictions = () => {
-  const [tableRows, setTableRows] = useState([
-    {  username: 'Mathinez', country: 'nations league', teams: 'netherland vs italy', prediction: 'draw/away'},
-    {  username: 'Mathinez', country: 'nations league', teams: 'croatia vs spain', prediction: 'ov1.5'},
-    {  username: 'Mathinez', country: 'afcon', teams: 'congo vs mali', prediction: 'draw/away'},
-    {  username: 'Mathinez', country: 'afcon', teams: 'uganda vs algeria', prediction: 'ov 1.5'},
-    {  username: 'Mathinez', country: 'afcon', teams: 'Sierra Leone vs nigeria', prediction: '0v 1.5'},
-    {  username: 'Mathinez', country: 'concacaf', teams: 'panama vs mexico', prediction: 'ov1.5'},
-    {  username: 'Mathinez', country: 'friendlies', teams: 'venezuela vs guatemala', prediction: 'draw/home'},
-    {  username: 'Mathinez', country: 'friendlies', teams: 'singapore vs solomon island', prediction: 'draw/home'},
-    {  username: 'Mathinez', country: 'friendlies', teams: 'egypt vs south sudan', prediction: 'home'}
-  ]);
+  const [tableRows, setTableRows] = useState(retrieveTableRows());
 
   const  [ showAddTable, setShowAddTable ] = useState(false)
+
+
+
+  useEffect(()=>{
+    localStorage.setItem('tableRows', JSON.stringify(tableRows))
+  },[tableRows])
+
 
   const handleNewRow = ( newRow) => {
     setTableRows([...tableRows, newRow])
