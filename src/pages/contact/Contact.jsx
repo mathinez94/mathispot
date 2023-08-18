d3d9d728318d95a6b24b941cc9edc79643d9ef85
Mathinez94 <audumathias@gmail.com>
2023-08-18 18:15:57 +0100

    added whatsapp link

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
      <Link><div className='contact-icons'><FaInstagram className='icons'/><p>Instagram</p></div></Link> 
       <Link><div className='contact-icons'><FaTelegramPlane className='icons'/><p>Telegram</p></div> </Link>
       <Link><div className='contact-icons'><FaTwitter  className='icons'/><p>Twitter</p></div></Link> 
-      <Link><div className='contact-icons'><FaWhatsapp className='icons'/><p>Whatsapp</p></div></Link>
+      <Link  to='https://wa.me/2348104849150' target='blank'><div className='contact-icons'><FaWhatsapp className='icons'/><p>Whatsapp</p></div></Link>
       <Link><div className='contact-icons'><FaFacebook className='icons'/><p>Facebook</p></div></Link>
     </div>
   )
